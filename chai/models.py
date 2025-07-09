5e594aa14e784e54f5de4e6ee677b506e0c01910
ilokeshrao <ly0367578@gmail.com>
2025-07-09 14:37:30 +0530

    admin super power

--- chai/models.py
+++ chai/models.py
@@ -1,5 +1,6 @@
 from django.db import models 
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -16,5 +17,46 @@ class ChaiVarity(models.Model):
     date_added = models.DateTimeField(default=timezone.now)
     type =models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
     desc =models.TextField(default='')
+
     def __str__(self):
       return self.name
+
+# one to many 
+
+class ChaiReview(models.Model):
+   chai =models.ForeignKey(ChaiVarity, on_delete=models.CASCADE,related_name='reviews')
+   user =models.ForeignKey(User, on_delete=models.CASCADE)
+   rating  = models.IntegerField()
+   comment = models.TextField()
+   date_added = models.DateTimeField(default=timezone.now)
+
+   def __str__(self):
+    return  f'{self.user.username} review for {self.chai.name}'
+   
+
+   # many to many 
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varities = models.ManyToManyField(ChaiVarity, related_name='stores')
+    
+    def __str__(self):
+      return self.name
+    
+
+    # one to one 
+
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVarity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.chai.name}'
+
+    
+
+
+   
+
